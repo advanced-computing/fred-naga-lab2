d0d2af83191bcd9f8513fe561c20f6bd615b1ef3
Naga <ml1234@columbia.edu>
2025-04-17 22:03:19 -0400

    update streamlit

--- Home.py
+++ Home.py
@@ -5,16 +5,17 @@ st.set_page_config(page_title="Predictive Analytics for Item-Level Gross Profits
                    page_icon="🏠")
 st.sidebar.header("Fred & Naga")
 
+st.title('🍺   🍷   🥂   🥃   🍶   🍸   🍹   🍾   🧉   🍻')
 st.title('Predictive Analytics for Item-Level Gross Profits in Iowa Liquor Stores')
 
-st.header('Problem',divider=True)
+st.header('🍺 Problem',divider=True)
 st.markdown('''
             This project takes maps of construction applications and prices of sold properties 
             in NYC and combines them to understand the relationsship between the amount of 
             housing construction and the property prices.
             ''')
 
-st.header('Data',divider=True)
+st.header('🍷 Data',divider=True)
 st.markdown('''
             **Research Questions**
             - What areas of the city (borough, community district, etc.) are seeing the most construction of housing? How many units can be expected?
@@ -25,7 +26,7 @@ st.markdown('''
             - [Prices of Sold Properties](https://data.cityofnewyork.us/City-Government/NYC-Citywide-Annualized-Calendar-Sales-Update/w2pb-icbu/about_data)
             ''')
 
-st.header('Next Steps',divider=True)
+st.header('🍹 Next Steps',divider=True)
 st.markdown('''
             **Research Questions**
             - What areas of the city (borough, community district, etc.) are seeing the most construction of housing? How many units can be expected?
